0e64d44dc79f4dbbfcc37ad19b12e620b03e5f2b
exmonprojeto <projeto_exmon@proton.me>
2023-11-18 02:41:26 -0300

    createuserchangeset_testando_import_libs_criptografia_senha

--- lib/bankcursor/users/user.ex
+++ lib/bankcursor/users/user.ex
@@ -1,11 +1,12 @@
 defmodule Bankcursor.Users.User do
     use Ecto.Schema
     import Ecto.Changeset
+    alias Ecto.Changeset
 
-    @required_params [:name, :password_hash, :email, :cep]
 
     schema "users" do
         field :name, :string
+        field :password, :string, virtual: true
         field :password_hash, :string 
         field :email, :string
         field :cep, :string
@@ -16,10 +17,18 @@ defmodule Bankcursor.Users.User do
 
     def changeset(user \\ %__MODULE__{}, params) do
         user
-        |> cast(params, @required_params)
-        |> validate_required(@required_params)
+        |> cast(params, [:name, :password, :email, :cep])
+        |> validate_required([:name, :password, :email, :cep])
         |> validate_length(:name, min: 3)
         |> validate_format(:email, ~r/@/)
         |> validate_length(:cep, is: 8)
+        |> add_password_hash()
     end
+
+    defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+        change(changeset, Pbkdf2.add_hash(password))
+    end
+
+    defp add_password_hash(changeset), do: changeset
+
 end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Bankcursor.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
