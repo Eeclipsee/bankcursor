27b6e88c64ceaa270dac58d6470ce3b17cb6822f
exmonprojeto <projeto_exmon@proton.me>
2023-11-22 16:25:41 -0300

    correção_error_insertuser_fromAPI

--- lib/bankcursor_web/controllers/users_json.ex
+++ lib/bankcursor_web/controllers/users_json.ex
@@ -1,10 +1,19 @@
-defmodule Bankcursor.UsersJson do
-    def create(%{user: user}) do
+defmodule BankcursorWeb.UsersJSON do
+    alias Bankcursor.Users.User
+    
+    def create(%{user: user}) do 
         %{
-            message: "User criado com sucesso!",
+            message: "User criado com sucesso",
+            data: data(user)
+        }
+    end
+
+    defp data(%User{} = user) do
+        %{
             id: user.id,
             cep: user.cep,
-            email: user.email
+            email: user.email,
+            name: user.name 
         }
     end
 end
