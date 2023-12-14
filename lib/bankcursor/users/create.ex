e07b730ef65bfa1131182074d0786d5baedb78bf
exmonprojeto <projeto_exmon@proton.me>
2023-12-14 01:15:49 -0300

    refatoring_alias_viacep_create

--- lib/bankcursor/users/create.ex
+++ lib/bankcursor/users/create.ex
@@ -1,12 +1,12 @@
 defmodule Bankcursor.Users.Create do
     alias Bankcursor.Users.User
     alias Bankcursor.Repo
-    alias Bankcursor.ViaCep.Client as: ViaCepClient
+    alias Bankcursor.ViaCep.Client
 
     def call(%{"cep" => cep} = params) do
-        with {:ok, _result} <- ViaCepClient.call(cep) do
+        with {:ok, _result} <- Client.call(cep) do
             params
-            |> User.changeset() 
+            |> User.changeset()
             |> Repo.insert()
         end
     end
