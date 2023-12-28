86f9b2d93773c70162aee2edced5c7f59a8fb750
exmonprojeto <projeto_exmon@proton.me>
2023-12-28 02:33:04 -0300

    create_test_users_controller

---
+++ test/bankcursor_web/users_controller_test.exs
@@ -0,0 +1,26 @@
+defmodule BankcursorWeb.UsersControllerTest do
+  use BankcursorWeb.ConnCase
+
+  describe "create/2" do
+    test "successfully creates an user", %{conn: conn} do
+
+      params = %{
+        name: "João",
+        password: "123455",
+        email: "teste@teste",
+        cep: "12345636"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:created)
+
+      assert %{
+                "data" => %{"cep" => "12345636", "email" => "teste@teste", "id" => _id, "name" => "João"},
+                "message" => "User criado com sucesso"
+              } = response
+
+    end
+  end
+end
