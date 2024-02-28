b1ecda99b11840ca3c5adf7c398711abb230fbf5
Gabriel <projetoexmon@proton.me>
2024-02-28 07:04:49 -0300

    vefiry user create token

--- lib/bankcursor/users/verify.ex
+++ lib/bankcursor/users/verify.ex
@@ -1,10 +1,18 @@
-defmodule Bankcursor.Users.Verify do
+ defmodule Bankcursor.Users.Verify do
   alias Bankcursor.Users
   alias Users.User
 
   def call(%{"id" => id, "password" => password}) do
-    case Users.Get(id) do
-      {:ok, user} -> Pbkdf2.verify_pass(password, stored_hash)
+    case Users.get(id) do
+      {:ok, user} -> Pbkdf2.verify_pass(password, user.password_hash)
+      {:error, _} = error -> error
+    end
+  end
+
+  def verify(user, password) do
+    case Pbkdf2.verify_pass(password, user.password_hash) do
+      true -> {:ok, :valid_password}
+      false -> {:error, :unauthorized}
     end
   end
 end
