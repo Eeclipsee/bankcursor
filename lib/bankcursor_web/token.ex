c666c733873c73213c3d8d0f52341c56e837f253
Gabriel <projetoexmon@proton.me>
2024-02-28 17:57:59 -0300

    create token_user, verify_user

---
+++ lib/bankcursor_web/token.ex
@@ -0,0 +1,13 @@
+defmodule BankcursorWeb.Token do
+  alias Phoenix.Token
+  alias BankcursorWeb.Endpoint
+
+  @sign_salt "bankcursor_api"
+
+  def sign(user) do
+    Token.sign(Endpoint, @sign_salt, %{user_id: user.id})
+  end
+
+  def verify(token), do: Token.verify(Endpoint, @sign_salt, token)
+
+end
